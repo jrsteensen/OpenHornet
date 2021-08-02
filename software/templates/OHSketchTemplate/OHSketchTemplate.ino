2a7fc35aad57789abddb4b79e72e7d1a02966f66
L-Walker <65761897+L-Walker@users.noreply.github.com>
2021-08-02 12:34:12 +1000

    Revert "Merge branch 'v1.0.0-beta1' of https://github.com/jrsteensen/OpenHornet into v1.0.0-beta1"
    This reverts commit bb9ccea3eca8bb515d8b7432aedb34468ce0c404, reversing
    changes made to dd582f8f71374baa33c58818ef8e5fb72c222a67.

--- software/templates/OHSketchTemplate/OHSketchTemplate.ino
+++ software/templates/OHSketchTemplate/OHSketchTemplate.ino
@@ -11,146 +11,77 @@
  *
  *   Project OpenHornet
  *
- *   The OpenHornet Project is a F/A-18C OFP 13C Lot 20 1:1 Replica Simulator,
- *   consisting of a physical structure and electrical/software interfaces to a PC
- *   to be driven by Digital Combat Simulator (DCS).
- *
- *   ---------------------------------------------------------------------------------
- *
- *   This Project is released under the Creative Commons
+ *   This Project is released under the Creative Commons 
  *   Atribution - Non Comercal - Share Alike License.
- *
+ *   
  *   CC BY-NC-SA 3.0
- *
+ *   
  *   You are free to:
  *   - Share — copy and redistribute the material in any medium or format
  *   - Adapt — remix, transform, and build upon the material
  *   The licensor cannot revoke these freedoms as long as you follow the license terms.
- *
+ *   
  *   Under the following terms:
- *   - Attribution — You must give appropriate credit, provide a link to the license,
- *     and indicate if changes were made. You may do so in any reasonable manner,
+ *   - Attribution — You must give appropriate credit, provide a link to the license, 
+ *     and indicate if changes were made. You may do so in any reasonable manner, 
  *     but not in any way that suggests the licensor endorses you or your use.
  *   - NonCommercial — You may not use the material for commercial purposes.
- *   - ShareAlike — If you remix, transform, or build upon the material,
+ *   - ShareAlike — If you remix, transform, or build upon the material, 
  *     you must distribute your contributions under the same license as the original.
- *
- *   No additional restrictions — You may not apply legal terms or technological
+ *     
+ *   No additional restrictions — You may not apply legal terms or technological 
  *   measures that legally restrict others from doing anything the license permits.
- *
+ *   
  *   More Information about the license can be found under:
  *   https://creativecommons.org/licenses/by-nc-sa/3.0/
- *
- *   ---------------------------------------------------------------------------------
- *
- *   The OpenHornet Software is based on DCS-BIOS
- *   You can find more information here: http://dcs-bios.a10c.de
- *
- *   DCS-BIOS is released under the following terms:
- *   https://github.com/dcs-bios/dcs-bios/blob/develop/DCS-BIOS-License.txt
- *
- *   ---------------------------------------------------------------------------------
- *
+ *   
+ *   
  *   This Project uses Doxygen as a documentation generator.
  *   Please use Doxigen capable comments.
- *
+ *   
  **************************************************************************************/
 
 /**
- * @file OHSketchTemplate.ino
- * @author Balz Reber
- * @date 29.12.2019
- * @version u.0.0.1 (untested)
- * @warning This sketch is based on a wiring diagram, and was not yet tested on hardware
- * @brief This is the OpenHornet Sketch Template
+ * @file <sketchName.ino>
+ * @author <Author Name>
+ * @date <Date>
+ * @brief <Very Brief Description>
  *
- * @details This is the Open Hornet Sketch Template. It should be used as a starting point for every new sketch.
- * Please copy the whole OHSketchTemplate folder to start. As it also contains some test skip files needed for travis.
- * 
- *  * **Intended Board:**
- * ABSIS XYZ
- * 
- * **Wiring diagram:**
- * 
- * PIN | Function
- * --- | ---
- * 1   | function 1
- * 2   | function 2
- * 3   | function 3
- * 
+ * <Put a more detailed description of the sketch here>
  */
-
-/**
- * Set DCS Bios to use irq serial
- */
-#define DCSBIOS_IRQ_SERIAL
-
-/**
- * DCS Bios library include
- */
-#include "DcsBios.h"
 
 
 /**
 * Arduino Setup Function
-*
+* 
 * Arduino standard Setup Function. Code who should be executed
 * only once at the programm start, belongs in this function.
 */
 void setup() {
 
-  // Run DCS Bios setup function
-  DcsBios::setup();
-
 }
 
 /**
 * Arduino Loop Function
-*
+* 
 * Arduino standard Loop Function. Code who should be executed
 * over and over in a loop, belongs in this function.
 */
 void loop() {
-
-  //Run DCS Bios loop function
-  DcsBios::loop();
-
+  
 }
 
 /**
 * A brief description on a single line, ended by a period or blank line.
 * 
 * A longer comment, which may stretch over several lines and may include other things like:
-* Lists:
-* - list points
-* + nested list points
-* - more list points
-* 
-* # Headers Level 1
-* ## Headers Level 2
-* ### Headers Level 3
-* 
-* > Block quotes
-* 
-* **Emphasis**
-* _Emphasis_
-* 
-* `code()`
-* 
-* even Tables are supported:
-* First Header  | Second Header
-* ------------- | -------------
-* Content Cell  | Content Cell 
-* Content Cell  | Content Cell 
-* 
-* Links:
-* [More about markdown support](http://www.doxygen.nl/manual/markdown.html)
+* - a list like this
+* - special markup like below
 * 
 * @param myParam1 Description of 1st parameter.
 * @param myParam2 Description of 2nd parameter.
 * @returns Description of returned value.
 */
 int sampleFunction(int myParam1, int myParam2) {
-  int myReturn;
   return myReturn;
 }
